b5edc24509a8a85b2dbd5134cade82fedfd3239c
Chen Lin <chenlin3@illinois.edu>
2020-03-21 19:07:21 -0500

    Added axios and api.service file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: () => import('@/components/HelloWorld')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login')
     }
   ]
 })
