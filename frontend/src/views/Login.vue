b0a150724b62f9e418d8e529c3c74f725e90e07f
Chen Lin <chenlin3@illinois.edu>
2020-03-21 19:07:44 -0500

    Added login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container page">
+    <div class="col-md-6 offset-md-3">
+      <h1>Sign in</h1>
+      <form @submit.prevent="onSubmit(username, password)">
+        <fieldset class="form-group">
+          <input class="form-control form-control-lg"
+                 type="text"
+                 v-model="username"
+                 placeholder="Username" />
+        </fieldset>
+        <fieldset class="form-group">
+          <input class="form-control form-control-lg"
+                 type="password"
+                 v-model="password"
+                 placeholder="Password" />
+        </fieldset>
+        <button class="btn btn-lg btn-primary">
+          Sign in
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit(username, password) {
+      console.log(username + ' ' + password)
+    }
+  }
+}
+</script>
