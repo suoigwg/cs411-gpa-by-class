1b91e0aefb00027a44f8a3ca9a5dd2d8a2e4d018
Chen Lin <chenlin3@illinois.edu>
2020-04-09 19:03:32 -0500

    project cleanup

--- courses/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import CoursesSerializer
-from .models import Courses
-
-
-class CoursesView(generics.ListCreateAPIView):
-    serializer_class = CoursesSerializer
-    queryset = Courses.objects.all()
