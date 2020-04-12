a7f087848dca59ede84844e5c060a8a43686d4d3
Chen Lin <chenlin3@illinois.edu>
2020-04-12 16:46:50 -0500

    Implemented prereq parsing

--- prereq/scraper.py
+++ prereq/scraper.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
+from common import write_to_file, read_json_from_file, DATA_FOLDER
 import urllib3
 import os
-import json
 
 BASE_URL = "https://courses.illinois.edu/schedule/2020/spring"
-DATA_FOLDER = "data/"
-COURSE_MAP_FILE_NAME = "course_map.txt"
-PREREQ_FILE_NAME = "prereqs.txt"
+COURSE_MAP_FILE_PATH = DATA_FOLDER + "course_map.txt"
+PREREQ_FILE_PATH = DATA_FOLDER + "prereqs.txt"
 
 
 def get_soup(url, browser):
@@ -76,38 +75,18 @@ def get_all_prereqs(course_map, browser):
     return prereqs
 
 
-def write_to_file(content, path, replace=False):
-    if not replace and os.path.exists(path):
-        print(f'{path} already exists')
-        return
-    else:
-        print(f'Writing: {path}')
-        with open(path, 'w') as f:
-            f.write(json.dumps(content))
-
-
-def read_json_from_file(path):
-    if os.path.exists(path):
-        print(f'Reading: {path}')
-        with open(path, 'r') as f:
-            content = f.read()
-            return json.loads(content)
-    else:
-        print(f'File: {path} does not exist')
-
-
 if __name__ == "__main__":
     http = urllib3.PoolManager()
     course_codes = get_course_codes(http)
-    course_map_path = DATA_FOLDER + COURSE_MAP_FILE_NAME
-    if not os.path.exists(course_map_path):
+    if not os.path.exists(COURSE_MAP_FILE_PATH):
         print('Course map does not exist')
         course_map = get_course_numbers(course_codes, http)
-        write_to_file(course_map, course_map_path)
+        write_to_file(course_map, COURSE_MAP_FILE_PATH)
     else:
         print('Course map exists')
-        course_map = read_json_from_file(course_map_path)
-    prereqs = get_all_prereqs(course_map, http)
-
-    prereq_file_path = DATA_FOLDER + PREREQ_FILE_NAME
-    write_to_file(prereqs, prereq_file_path)
+        course_map = read_json_from_file(COURSE_MAP_FILE_PATH)
+    if not os.path.exists(PREREQ_FILE_PATH):
+        prereqs = get_all_prereqs(course_map, http)
+        write_to_file(prereqs, PREREQ_FILE_PATH)
+    else:
+        print('Prereq file already exists')
